712df219891d9bb89d7ed15afd6bdd5697fb9fe1
kuliahumy26-byte <kuliahumy26@gmail.com>
2026-02-24 20:20:00 +0700

    Final commit project 2

--- Praktikum 1.1 .cpp
+++ Praktikum 1.1 .cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int main (){ //mulai
+    int p, l, luas; //deklarasi
+
+    //langkah 2
+    cout << "masukkan panjang = ";
+    cin >> p;
+
+    //langkah 3
+    cout << "masukkan lebar = ";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "hasilnya = " << luas  << endl;
+    cout << "terima kasih";
+} //selesai
